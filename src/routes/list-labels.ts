bb7a3863761792a9bc8d72246ba5c10fc289a384
Rob Anderson <rob.anderson@ncl.ac.uk>
2020-01-27 15:29:58 +0000

    fix: only send labels which are on cards

--- src/routes/list-labels.ts
+++ src/routes/list-labels.ts
@@ -4,5 +4,19 @@ import { RouteContext } from '../types'
 // GET /data/labels ~ get all labels
 //
 export default async ({ sendData, redis }: RouteContext) => {
-  sendData(await redis.getJson('labels'))
+  // Grab the cards and labels from redis
+  const cards = await redis.getJson('cards')
+  const labels = await redis.getJson('labels')
+
+  // Build a set of label ids that have been used
+  const usedLabels = new Set<string>()
+
+  for (const card of cards) {
+    for (const labelId of card.idLabels) {
+      usedLabels.add(labelId)
+    }
+  }
+
+  // Send back the labels which have been used by at least one card
+  sendData(labels.filter((label: any) => usedLabels.has(label.id)))
 }
